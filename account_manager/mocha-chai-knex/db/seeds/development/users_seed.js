f273b9159116cd57faa5f9a3a721525e91fa6c68
Maple-Lazuli <the.nostra.tymus@gmail.com>
2020-11-21 02:07:10 +0900

    added knex seeds

--- account_manager/mocha-chai-knex/db/seeds/development/users_seed.js
+++ account_manager/mocha-chai-knex/db/seeds/development/users_seed.js
@@ -2,34 +2,59 @@ exports.seed = function(knex, Promise) {
   return knex('users').del() // Deletes ALL existing entries
     .then(function() { // Inserts seed entries one by one in series
       return knex('users').insert({
-        userID: "user1"
-        Fname:"Philip"
-        Lname:"Jim"
-        
+        "firstName": "Stevens",
+        "lastName": "Mitchel",
+        "userid": "user24",
+        "sessionid": "user24",
+        "paygrade": "E-2",
+        "rank": "Amn",
+        "gender": "Female",
+        "rankInt": 2,
+        "AFSC": "1Q251A",
+        "unit": "6101st Air Base Wing",
+        "DOR": "1-5-1998"
       });
     }).then(function () {
-      return knex('shows').insert({
-        name: 'Game of Thrones',
-        channel: 'HBO',
-        genre: 'Fantasy',
-        rating: 5,
-        explicit: true
+      return knex('users').insert({
+        "firstName": "Athanasios",
+        "lastName": "Peloquin",
+        "userid": "user26",
+        "sessionid": "user26",
+        "paygrade": "E-9",
+        "rank": "CMSgt",
+        "gender": "Male",
+        "rankInt": 9,
+        "AFSC": "1Q251A",
+        "unit": "5900th Wing 5900th Composite Wing",
+        "DOR": "7-1-2017"
       });
     }).then(function () {
-      return knex('shows').insert({
-        name: 'South Park',
-        channel: 'Comedy Central',
-        genre: 'Comedy',
-        rating: 4,
-        explicit: true
+      return knex('users').insert({
+        "firstName": "Jeffrie",
+        "lastName": "Caesar",
+        "userid": "user22",
+        "sessionid": "user22",
+        "paygrade": "E-4",
+        "rank": "SrA",
+        "gender": "Female",
+        "rankInt": 4,
+        "AFSC": "1Q251A",
+        "unit": "4950th Test Wing",
+        "DOR": "2-7-2020"
       });
     }).then(function () {
-      return knex('shows').insert({
-        name: 'Mad Men',
-        channel: 'AMC',
-        genre: 'Drama',
-        rating: 3,
-        explicit: false
+      return knex('users').insert({
+        "firstName": "Vida",
+        "lastName": "Romain",
+        "userid": "user21",
+        "sessionid": "user21",
+        "paygrade": "E-3",
+        "rank": "A1C",
+        "gender": "Female",
+        "rankInt": 3,
+        "AFSC": "1Q251A",
+        "unit": "4070th Support Wing",
+        "DOR": "1-15-2008"
       });
     });
 };
