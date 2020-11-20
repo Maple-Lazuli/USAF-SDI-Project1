ddac425bcae54d8012dddf4f5661fd76cdf90fe1
Maple-Lazuli <the.nostra.tymus@gmail.com>
2020-11-21 02:57:09 +0900

    Server has data , knex is operational, and mocha is installed.

--- account_manager/mocha-chai-knex/routes/index.js
+++ account_manager/mocha-chai-knex/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// *** GET all users *** //
+router.get('/alldeusers', function(req, res, next) {
+  res.send('send users back');
 });
+
 
 module.exports = router;
